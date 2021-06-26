fc672835567f03cd04bcec6314a06d9f788b6ea4
Mansour-Aldundur <mansour.aldundur@tuwaiq.edu.sa>
2021-06-26 23:08:48 +0300

    add: Project Files

---
+++ ObserverDrill/ObserverDrill/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObserverDrill
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var alarm = new Alarm();
+            alarm.AddWatcher(new FireStation());
+            alarm.AddWatcher(new PoliceStation());
+            alarm.Notify();
+        }
+    }
+
+    public class Alarm
+    {
+        private readonly List<IWatcher<Alarm>> watchers = new();
+
+        public void AddWatcher(IWatcher<Alarm> alarmWatcher)
+        {
+            watchers.Add(alarmWatcher);
+        }
+
+        public void RemoveWatcher(IWatcher<Alarm> alarmWatcher)
+        {
+            watchers.Remove(alarmWatcher);
+        }
+
+        public void Notify()
+        {
+            foreach (var w in watchers) w.Alert(this);
+        }
+    }
+
+    public interface IWatcher<T>
+    {
+        public void Alert(T value);
+    }
+
+    public class FireStation : IWatcher<Alarm>
+    {
+        public void Alert(Alarm value)
+        {
+            Console.WriteLine("Fire station Responding");
+        }
+    }
+
+    public class PoliceStation : IWatcher<Alarm>
+    {
+        public void Alert(Alarm value)
+        {
+            Console.WriteLine("Police station Responding");
+        }
+    }
+}

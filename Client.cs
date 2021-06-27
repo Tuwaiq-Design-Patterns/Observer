c7f79e816252445896de0d7b99c300b3fcb42b4f
1Riyad <Riyad.Alghamdi@tuwaiq.edu.sa>
2021-06-27 04:02:28 -0400

    initial commit

---
+++ Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Observer
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            var app = new TawakkalnaApp();
+            Console.WriteLine("Users are signing up");
+            Console.WriteLine("-----------------------------");
+
+            var userA = new Citizen();
+            app.SignUp(userA);
+
+            var userB = new Alien();
+            app.SignUp(userB);
+
+            Console.WriteLine("\nUsers are receiving updates");
+            Console.WriteLine("-----------------------------");
+
+            app.FeatureUpdate();
+            app.BugFixUpdate();
+
+            app.DeleteAccount(userB);
+            Console.WriteLine($"\nAfter {userB.GetType().Name} user has deleted his account, now one user is receiving updates");
+            Console.WriteLine("-----------------------------");
+            app.BugFixUpdate();
+        }
+    }
+}
---
+++ Publisher.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Observer
+{
+
+    public interface ITawakkalnaApp
+    {
+        void SignUp(ITawakkalnaUser user);
+
+        void DeleteAccount(ITawakkalnaUser user);
+
+        void Notify();
+    }
+
+
+
+
+    public class TawakkalnaApp : ITawakkalnaApp
+    {
+        public double version { get; set; }
+
+        private List<ITawakkalnaUser> _tawakkalnaUsers = new List<ITawakkalnaUser>();
+
+        public void SignUp(ITawakkalnaUser user)
+        {
+            Console.WriteLine("Tawakkalna: user just Signed Up");
+            this._tawakkalnaUsers.Add(user);
+        }
+
+        public void DeleteAccount(ITawakkalnaUser user)
+        {
+            this._tawakkalnaUsers.Remove(user);
+            Console.WriteLine($"\nTawakkalna: a '{user.GetType().Name}' user has just deleted his Account");
+        }
+
+        // Trigger an update in each subscriber.
+        public void Notify()
+        {
+            Console.WriteLine("Tawakkalna: Notifying users...");
+
+            foreach (var user in _tawakkalnaUsers)
+            {
+                user.Update(this);
+            }
+        }
+
+        public void FeatureUpdate()
+        {
+            this.version += 1;
+
+            Console.WriteLine("Tawakkalna: The new Feature of Tawakkalna has been released with version #" + this.version.ToString());
+            this.Notify();
+        }
+
+        public void BugFixUpdate()
+        {
+            this.version += 0.1;
+
+            Console.WriteLine("Tawakkalna: There was bugFix of Tawakkalna and  a new release  has been published with version # " + this.version.ToString().Substring(0, 3));
+            this.Notify();
+        }
+    }
+}
